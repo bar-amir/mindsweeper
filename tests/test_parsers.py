6b503510d98e98e8cc96e6635e471f1bb0dd1b00
Bar Amir <baramir93@gmail.com>
2020-05-27 12:48:25 +0300

    Tests amd requirements.txt

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -8,10 +8,6 @@ HOST = '127.0.0.1'
 PORT = 8000
 
 
-def test_parsers_error(tmp_path):
-    pass
-
-
 def test_parse(tmp_path):
     path = tmp_path / 'raw.bin'
     with open(path, 'wb') as f:
@@ -19,6 +15,4 @@ def test_parse(tmp_path):
         f.write(bson.encode(msg))
     msg = parsers.parse('pose', path)
     assert msg['status'] == 'ready'
-    runner = CliRunner()
-    result = runner.invoke(parsers.parse_command, ['pose', path])
-    assert result.exit_code == 0
+
